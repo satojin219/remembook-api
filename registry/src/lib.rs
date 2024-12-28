5f677359bd52bba31168d72792af8ad0215d0fd4
satojin219 <renzuoteng142@gmail.com>
2024-12-28 17:49:27 +0900

    feat: book_repositoryをregistryに登録

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -3,8 +3,10 @@ use std::sync::Arc;
 use adapter::database::ConnectionPool;
 use adapter::redis::RedisClient;
 use adapter::repository::auth::AuthRepositoryImpl;
+use adapter::repository::book::BookRepositoryImpl;
 use adapter::repository::user::UserRepositoryImpl;
 use kernel::repository::auth::AuthRepository;
+use kernel::repository::book::BookRepository;
 use kernel::repository::user::UserRepository;
 use shared::config::AppConfig;
 
@@ -12,6 +14,7 @@ use shared::config::AppConfig;
 pub struct AppRegistryImpl {
     auth_repository: Arc<dyn AuthRepository>,
     user_repository: Arc<dyn UserRepository>,
+    book_repository: Arc<dyn BookRepository>,
 }
 
 impl AppRegistryImpl {
@@ -26,10 +29,12 @@ impl AppRegistryImpl {
             app_config.auth.ttl,
         ));
         let user_repository = Arc::new(UserRepositoryImpl::new(pool.clone()));
+        let book_repository = Arc::new(BookRepositoryImpl::new(pool.clone()));
 
         Self {
             auth_repository,
             user_repository,
+            book_repository,
         }
     }
 }
@@ -37,6 +42,7 @@ impl AppRegistryImpl {
 pub trait AppRegistryExt {
     fn auth_repository(&self) -> Arc<dyn AuthRepository>;
     fn user_repository(&self) -> Arc<dyn UserRepository>;
+    fn book_repository(&self) -> Arc<dyn BookRepository>;
 }
 
 impl AppRegistryExt for AppRegistryImpl {
@@ -46,6 +52,10 @@ impl AppRegistryExt for AppRegistryImpl {
 
     fn user_repository(&self) -> Arc<dyn UserRepository> {
         self.user_repository.clone()
+    }
+
+    fn book_repository(&self) -> Arc<dyn BookRepository> {
+        self.book_repository.clone()
     }
 }
 
