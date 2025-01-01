a6430b5d8ae24c25ab08591a7a637f50ac9d3664
satojin219 <renzuoteng142@gmail.com>
2025-01-02 08:37:42 +0900

    feat: repositoryをregistryで登録

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -5,8 +5,11 @@ use adapter::redis::RedisClient;
 use adapter::repository::auth::AuthRepositoryImpl;
 use adapter::repository::book::BookRepositoryImpl;
 use adapter::repository::user::UserRepositoryImpl;
+use kernel::repository::answer::AnswerRepository;
 use kernel::repository::auth::AuthRepository;
 use kernel::repository::book::BookRepository;
+use kernel::repository::question::QuestionRepository;
+use kernel::repository::summary::SummaryRepository;
 use kernel::repository::user::UserRepository;
 use shared::config::AppConfig;
 
@@ -15,6 +18,9 @@ pub struct AppRegistryImpl {
     auth_repository: Arc<dyn AuthRepository>,
     user_repository: Arc<dyn UserRepository>,
     book_repository: Arc<dyn BookRepository>,
+    summary_repository: Arc<dyn SummaryRepository>,
+    question_repository: Arc<dyn QuestionRepository>,
+    answer_repository: Arc<dyn AnswerRepository>,
 }
 
 impl AppRegistryImpl {
@@ -30,11 +36,16 @@ impl AppRegistryImpl {
         ));
         let user_repository = Arc::new(UserRepositoryImpl::new(pool.clone()));
         let book_repository = Arc::new(BookRepositoryImpl::new(pool.clone()));
-
+        let summary_repository = Arc::new(SummaryRepositoryImpl::new(pool.clone()));
+        let question_repository = Arc::new(QuestionRepositoryImpl::new(pool.clone()));
+        let answer_repository = Arc::new(AnswerRepositoryImpl::new(pool.clone()));
         Self {
             auth_repository,
             user_repository,
             book_repository,
+            summary_repository,
+            question_repository,
+            answer_repository,
         }
     }
 }
@@ -43,6 +54,9 @@ pub trait AppRegistryExt {
     fn auth_repository(&self) -> Arc<dyn AuthRepository>;
     fn user_repository(&self) -> Arc<dyn UserRepository>;
     fn book_repository(&self) -> Arc<dyn BookRepository>;
+    fn summary_repository(&self) -> Arc<dyn SummaryRepository>;
+    fn question_repository(&self) -> Arc<dyn QuestionRepository>;
+    fn answer_repository(&self) -> Arc<dyn AnswerRepository>;
 }
 
 impl AppRegistryExt for AppRegistryImpl {
@@ -56,6 +70,18 @@ impl AppRegistryExt for AppRegistryImpl {
 
     fn book_repository(&self) -> Arc<dyn BookRepository> {
         self.book_repository.clone()
+    }
+
+    fn summary_repository(&self) -> Arc<dyn SummaryRepository> {
+        self.summary_repository.clone()
+    }
+
+    fn question_repository(&self) -> Arc<dyn QuestionRepository> {
+        self.question_repository.clone()
+    }
+
+    fn answer_repository(&self) -> Arc<dyn AnswerRepository> {
+        self.answer_repository.clone()
     }
 }
 
