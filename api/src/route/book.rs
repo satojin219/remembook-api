92dc098117893e871f54f250b5a95c0a88da6fd3
satojin219 <renzuoteng142@gmail.com>
2025-01-02 20:18:49 +0900

    feat: routeにsummaryメソッドを追加

--- api/src/route/book.rs
+++ api/src/route/book.rs
@@ -4,14 +4,26 @@ use axum::{
 };
 use registry::AppRegistry;
 
-use crate::handler::book::{delete_book, register_book, show_book, show_book_list};
+use crate::handler::{
+    book::{delete_book, register_book, show_book, show_book_list},
+    summary::{answer_question, create_summary, delete_summary, get_question, update_summary},
+};
 
 pub fn build_book_routers() -> Router<AppRegistry> {
-    let books_routers = Router::new()
+    let books_router = Router::new()
         .route("/", post(register_book))
         .route("/", get(show_book_list))
         .route("/:book_id", get(show_book))
         .route("/:book_id", delete(delete_book));
 
-    Router::new().nest("/books", books_routers)
+    let summary_router = Router::new()
+        .route("/", post(create_summary))
+        .route("/:summary_id", put(update_summary))
+        .route("/:summary_id", delete(delete_summary))
+        .route("/:summary_id/question", get(get_question))
+        .route("/:summary_id/answer/:question_id", post(answer_question));
+
+    Router::new()
+        .nest("/books", books_router)
+        .nest("/books/:book_id/summary", summary_router)
 }
