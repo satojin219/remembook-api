403f2a333886d7b1d6a467bfaba76af78ac80482
satojin219 <renzuoteng142@gmail.com>
2025-02-16 21:40:05 +0900

    refactor: リクエストからuser_idを削除

--- api/src/model/user.rs
+++ api/src/model/user.rs
@@ -1,12 +1,9 @@
 use garde::Validate;
-use kernel::model::id::UserId;
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct AddCoinRequest {
-    #[garde(skip)]
-    pub user_id: UserId,
     #[garde(range(min = 1))]
     pub amount: i32,
     #[garde(length(min = 1))]
